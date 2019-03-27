53842fba1d86fca1ce41c5c8025153b850dd2e44
Howie lynch <THEGRINCH1985@GMAIL.COM>
2019-03-27 10:50:47 +0000

    Edited Colours aded Non Functionality Search Bar

--- BladeRunner/app/src/main/java/com/example/phil/bladerunner/MainActivity.java
+++ BladeRunner/app/src/main/java/com/example/phil/bladerunner/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.phil.bladerunner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SearchView;
 
+import com.example.phil.bladerunner.adapters.ListViewAdapter;
 import com.example.phil.bladerunner.adapters.RecipeAdapter;
 import com.example.phil.bladerunner.clients.RecipeRestClient;
 import com.example.phil.bladerunner.models.Recipes;
@@ -21,16 +25,53 @@ import cz.msebera.android.httpclient.message.BasicHeader;
 public class MainActivity extends AppCompatActivity {
 
     private ListView recipeList;
+    // Declare Variables
+
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    Recipes[] recipeNameList;
+    ArrayList<Recipes> arraylist = new ArrayList<Recipes>();
+
+
+    private ArrayAdapter adapterSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText theFilter = (EditText) findViewById(R.id.simpleSearchView);
 
         getRecipes();
+        recipeNameList = new Recipes[]{};
+        recipeList = (ListView) findViewById(R.id.list_recipes);
+
+        for (int i =0; i < recipeNameList.length; i ++){
+
+            //Recipes r =new Recipes();
+            //arraylist.add(r);
+        }
+
+        adapter = new ListViewAdapter(this,arraylist);
+
+        recipeList.setAdapter(adapter);
+
+        editsearch = (SearchView) findViewById(R.id.simpleSearchView);
+        //editsearch.setOnQueryTextListener((SearchView.OnQueryTextListener) this);
+
     }
 
-    private void getRecipes() {
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
+    }
+
+    public void getRecipes() {
         List<Header> headers = new ArrayList<Header>();
         headers.add(new BasicHeader("Accept", "application/json"));
 
@@ -53,5 +94,30 @@ public class MainActivity extends AppCompatActivity {
                         recipeList.setAdapter(recipeAdapter);
                     }
                 });
+    }
+    public ListView populate() {
+        List<Header> headers = new ArrayList<Header>();
+        headers.add(new BasicHeader("Accept", "application/json"));
+
+        RecipeRestClient.get(MainActivity.this, "all", headers.toArray(new Header[headers.size()]),
+                null, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        ArrayList<Recipes> recipeArray = new ArrayList<Recipes>();
+                        RecipeAdapter recipeAdapter = new RecipeAdapter(MainActivity.this, recipeArray);
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                recipeAdapter.add(new Recipes(response.getJSONObject(i)));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        recipeList = (ListView) findViewById(R.id.list_recipes);
+                        recipeList.setAdapter(recipeAdapter);
+                    }
+                });
+        return recipeList;
     }
 }
---
+++ BladeRunner/app/src/main/java/com/example/phil/bladerunner/adapters/ListViewAdapter.java
@@ -0,0 +1,84 @@
+package com.example.phil.bladerunner.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.phil.bladerunner.R;
+import com.example.phil.bladerunner.models.Recipes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class ListViewAdapter extends BaseAdapter {
+
+    // Declare Variables
+
+    Context mContext;
+    LayoutInflater inflater;
+    private List<Recipes> animalNamesList = null;
+    private ArrayList<Recipes> arraylist;
+
+    public ListViewAdapter(Context context, List<Recipes> animalNamesList) {
+        mContext = context;
+        this.animalNamesList = animalNamesList;
+        inflater = LayoutInflater.from(mContext);
+        this.arraylist = new ArrayList<Recipes>();
+        this.arraylist.addAll(animalNamesList);
+    }
+
+    public class ViewHolder {
+        TextView name;
+    }
+
+    @Override
+    public int getCount() {
+        return animalNamesList.size();
+    }
+
+    @Override
+    public Recipes getItem(int position) {
+        return animalNamesList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(final int position, View view, ViewGroup parent) {
+        final ViewHolder holder;
+        if (view == null) {
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.item_recipe, null);
+            // Locate the TextViews in listview_item.xml
+            holder.name = (TextView) view.findViewById(R.id.label_recipe_title);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        // Set the results into TextViews
+        holder.name.setText(animalNamesList.get(position).getRecipeName());
+        return view;
+    }
+
+    // Filter Class
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        animalNamesList.clear();
+        if (charText.length() == 0) {
+            animalNamesList.addAll(arraylist);
+        } else {
+            for (Recipes wp : arraylist) {
+                if (wp.getRecipeName().toLowerCase(Locale.getDefault()).contains(charText)) {
+                    animalNamesList.add(wp);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+}
